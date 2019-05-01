f78d97703a895253c4fcc7220dd06a45655a4739
Niklas Fiekas <niklas.fiekas@backscattering.de>
2019-05-01 20:51:06 +0200

    basic web push client usage

--- src/main.rs
+++ src/main.rs
@@ -2,10 +2,32 @@
 
 #![warn(rust_2018_idioms)]
 
+use serde::Deserialize;
 use std::net::SocketAddr;
 use structopt::StructOpt;
+use futures::compat::Future01CompatExt as _;
 use tide::{Context, EndpointResult};
+use tide::error::ResultExt;
 use tide::http::status::StatusCode;
+use web_push::{WebPushClient, WebPushMessageBuilder, SubscriptionInfo, VapidSignatureBuilder};
+use web_push::ContentEncoding::AesGcm;
+
+// TODO: UNWRAP
+
+#[derive(Deserialize, Debug)]
+struct PushRequest {
+    vapid: Vapid,
+    sub: SubscriptionInfo,
+    payload: String,
+    ttl: u32,
+}
+
+#[derive(Deserialize, Debug)]
+struct Vapid {
+    subject: String,
+    public: String,
+    private: String,
+}
 
 #[derive(StructOpt, Debug)]
 struct Opt {
@@ -13,19 +35,34 @@ struct Opt {
     #[structopt(long = "address", default_value = "127.0.0.1")]
     address: String,
     /// Listen on this port.
-    #[structopt(long = "port", default_value = "9000")]
+    #[structopt(long = "port", default_value = "9054")]
     port: u16,
 }
 
-async fn push(_cx: Context<()>) -> EndpointResult<StatusCode> {
-    Ok(StatusCode::BAD_REQUEST)
+async fn push(mut cx: Context<WebPushClient>) -> EndpointResult<StatusCode> {
+    let client = cx.app_data();
+    let req: PushRequest = await!(cx.body_json()).client_err()?;
+
+    let mut signature: VapidSignatureBuilder<'_> = unimplemented!();
+    signature.add_claim("sub", req.vapid.subject);
+
+    let mut builder = WebPushMessageBuilder::new(&req.sub).unwrap(); // XXX
+    builder.set_ttl(req.ttl);
+    builder.set_payload(AesGcm, req.payload.as_bytes());
+    builder.set_vapid_signature(signature.build().unwrap()); // XXX
+    let message = builder.build().unwrap(); // XXX
+
+    await!(client.send(message).compat()).unwrap(); // XXX
+    Ok(StatusCode::NO_CONTENT)
 }
 
 fn main() {
     let opt = Opt::from_args();
     let bind = SocketAddr::new(opt.address.parse().expect("valid address"), opt.port);
 
-    let mut app = tide::App::new(());
+    let client = WebPushClient::new().expect("push client");
+
+    let mut app = tide::App::new(client);
     app.at("/").post(push);
     app.serve(bind).expect("bind");
 }
