c869053a1db32c44e806edbbe3f7141878142823
Niklas Fiekas <niklas.fiekas@backscattering.de>
2019-05-01 21:25:20 +0200

    error handling

--- src/main.rs
+++ src/main.rs
@@ -7,28 +7,14 @@ use std::fs;
 use std::io::Cursor;
 use std::net::SocketAddr;
 use std::path::PathBuf;
+use std::time::Duration;
 use structopt::StructOpt;
 use futures::compat::Future01CompatExt as _;
-use tide::{Context, EndpointResult};
-use tide::error::ResultExt;
+use tide::{Context, Response};
+use tide::response::IntoResponse;
 use tide::http::status::StatusCode;
-use web_push::{WebPushClient, WebPushMessageBuilder, SubscriptionInfo, VapidSignatureBuilder};
+use web_push::{WebPushClient, WebPushMessageBuilder, SubscriptionInfo, VapidSignatureBuilder, WebPushError};
 use web_push::ContentEncoding::AesGcm;
-
-// TODO: UNWRAP
-
-struct App {
-    client: WebPushClient,
-    vapid: Vec<u8>,
-    subject: String,
-}
-
-#[derive(Deserialize, Debug)]
-struct PushRequest {
-    sub: SubscriptionInfo,
-    payload: String,
-    ttl: u32,
-}
 
 #[derive(StructOpt, Debug)]
 struct Opt {
@@ -47,20 +33,48 @@ struct Opt {
     port: u16,
 }
 
-async fn push(mut cx: Context<App>) -> EndpointResult<StatusCode> {
-    let req: PushRequest = await!(cx.body_json()).client_err()?;
+struct App {
+    client: WebPushClient,
+    vapid: Vec<u8>,
+    subject: String,
+}
+
+#[derive(Deserialize, Debug)]
+struct PushRequest {
+    sub: SubscriptionInfo,
+    payload: String,
+    ttl: u32,
+}
+
+struct PushError(WebPushError);
+
+impl From<WebPushError> for PushError {
+    fn from(err: WebPushError) -> PushError {
+        PushError(err)
+    }
+}
+
+impl IntoResponse for PushError {
+    fn into_response(self) -> Response {
+        "error".with_status(StatusCode::BAD_REQUEST).into_response()
+    }
+}
+
+async fn push(mut cx: Context<App>) -> Result<StatusCode, PushError> {
+    let req: PushRequest = await!(cx.body_json()).map_err(|_| WebPushError::BadRequest(None))?;
     let app = cx.app_data();
 
-    let mut signature = VapidSignatureBuilder::from_pem(Cursor::new(&app.vapid), &req.sub).unwrap();
+    let mut signature = VapidSignatureBuilder::from_pem(Cursor::new(&app.vapid), &req.sub)?;
     signature.add_claim("sub", app.subject.clone());
 
-    let mut builder = WebPushMessageBuilder::new(&req.sub).unwrap(); // XXX
+    let mut builder = WebPushMessageBuilder::new(&req.sub)?;
     builder.set_ttl(req.ttl);
     builder.set_payload(AesGcm, req.payload.as_bytes());
-    builder.set_vapid_signature(signature.build().unwrap()); // XXX
-    let message = builder.build().unwrap(); // XXX
+    builder.set_vapid_signature(signature.build()?);
+    let message = builder.build()?;
 
-    await!(app.client.send(message).compat()).unwrap(); // XXX
+    let timeout = Duration::from_secs(15);
+    await!(app.client.send_with_timeout(message, timeout).compat())?;
     Ok(StatusCode::NO_CONTENT)
 }
 
