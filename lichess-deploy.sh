e33ff833f7cd856989ac8d55ddfe0cfb6462f777
Niklas Fiekas <niklas.fiekas@backscattering.de>
2020-02-01 14:16:56 +0100

    add deploy script

---
+++ lichess-deploy.sh
@@ -0,0 +1,5 @@
+#!/bin/sh -e
+cargo +nightly build --release
+ssh "root@$1.lichess.ovh" mv /usr/local/bin/lila-push /usr/local/bin/lila-push.bak || (echo "first deploy on this server? set up service and comment out this line" && false)
+scp ./target/release/lila-push "root@$1.lichess.ovh":/usr/local/bin/lila-push
+ssh "root@$1.lichess.ovh" systemctl restart lila-push
